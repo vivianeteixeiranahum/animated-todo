6fca605cf637c066f53e0954371b20913138af78
vivianeteixeiranahum <vivianeteixeiranahum@gmail.com>
2024-07-17 15:27:11 -0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import MainScreen from './screens/main-screen'
+import Sidebar from './components/sidebar'
+
+const Drawer = createDrawerNavigator()
+
+const App = () => {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Main"
+      drawerContent={props => <Sidebar {...props} />}
+      screenOptions={{
+        headerShown: false,
+        drawerType: 'back',
+        overlayColor: '#00000000'
+      }}
+    >
+      <Drawer.Screen name="Main" component={MainScreen} />
+    </Drawer.Navigator>
+  )
+}
+
+export default App
